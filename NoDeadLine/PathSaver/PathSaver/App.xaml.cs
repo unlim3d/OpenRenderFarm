3b348e5f4c3af6b8934aa6eba9b5c7c004f48469
ArturPiskarev <31780872+ArturPiskarev@users.noreply.github.com>
2020-05-13 22:23:07 +0300

    NotifyIcon

--- NoDeadLine/PathSaver/PathSaver/App.xaml.cs
+++ NoDeadLine/PathSaver/PathSaver/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,18 @@ namespace PathSaver
             RenderPath,
             CollectPath
         }
+        
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            
+           
+            System.Windows.Forms.NotifyIcon nIcon = new System.Windows.Forms.NotifyIcon();
+            nIcon.Visible = true;
+            nIcon.Icon = SystemIcons.Application;
+            nIcon.ShowBalloonTip(5000, "Title", "Text", System.Windows.Forms.ToolTipIcon.Info);
+            //nIcon.Click += NIcon_Click;
+            nIcon.ContextMenuStrip= new System.Windows.Forms.ContextMenuStrip();
+            nIcon.ContextMenuStrip.Items.Add("Exit", null, this.MenuExit_Click);
+
             if (e.Args.Length == 1)
             {
                 if (e.Args[0] == "RenderPath")
@@ -45,6 +55,17 @@ namespace PathSaver
                     MainWindow wnd = new MainWindow();
                 }
             }
+
+            
+        }
+        void MenuExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void NIcon_Click(object sender, EventArgs e)
+        {
+            MainWindow.Visibility = Visibility.Visible;
+            MainWindow.WindowState = WindowState.Normal;
         }
     }
 }
